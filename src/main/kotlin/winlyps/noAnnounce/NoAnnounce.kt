8d2d66c9196713e54c23be0b38498c61845a66ec
Winlypse <nts3@o2.pl>
2024-10-22 13:32:20 +0200

    Changes

--- src/main/kotlin/winlyps/noAnnounce/NoAnnounce.kt
+++ src/main/kotlin/winlyps/noAnnounce/NoAnnounce.kt
@@ -6,9 +6,14 @@ class NoAnnounce : JavaPlugin() {
 
     override fun onEnable() {
         // Plugin startup logic
+        server.worlds.forEach { world ->
+            world.setGameRuleValue("announceAdvancements", "false")
+        }
+        logger.info("NoAnnounce plugin has been enabled.")
     }
 
     override fun onDisable() {
         // Plugin shutdown logic
+        logger.info("NoAnnounce plugin has been disabled.")
     }
 }
