7033922f2db59e8d84aaf5be512e0d8181febc55
Winlypse <nts3@o2.pl>
2024-10-22 13:32:11 +0200

    Initial commit

---
+++ src/main/kotlin/winlyps/noAnnounce/NoAnnounce.kt
@@ -0,0 +1,14 @@
+package winlyps.noAnnounce
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class NoAnnounce : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
